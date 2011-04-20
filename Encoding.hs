d248e3e35d271d013792f19b284f30706e209b88
znation <znation@gmail.com>
2011-04-19 22:11:36 -0700

    Revert "Byte encoding using Foreign.Storable and pointers"
    This reverts commit ce76484e2725ae674ddaa8779af04fd512f5015f.

--- Encoding.hs
+++ Encoding.hs
@@ -1,23 +1,19 @@
 module Encoding (byteEncode) where
 
+import qualified Data.Bits as B
+import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Word as W
-import qualified Foreign.Marshal as FM
-import qualified Foreign.Marshal.Utils as FU
-import qualified Foreign.Ptr as FP
 import qualified Foreign.Storable as FS
 
-byteEncode :: FS.Storable a => a -> [W.Word8]
+byteEncode :: Integral a => B.Bits a => FS.Storable a => a -> [W.Word8]
 byteEncode i = byteEncode' (FS.sizeOf i) i
 
-byteEncode' :: FS.Storable a => Int-> a -> [W.Word8]
+byteEncode' :: Integral a => B.Bits a => FS.Storable a => Int-> a -> [W.Word8]
 byteEncode' size i
     | size == 1 = [getByte i 0]
     | size == 2 = [(getByte i 0), (getByte i 1)]
     | size == 4 = [(getByte i 0), (getByte i 1), (getByte i 2), (getByte i 3)]
     | otherwise = []
 
-getByte :: FS.Storable a => a -> Int -> W.Word8
-getByte i idx = FM.unsafeLocalState (FU.with i (getByte' (idx * 8)))
-
-getByte' :: (FS.Storable a, FS.Storable b) => Int -> FP.Ptr a -> IO b
-getByte' idx i = FS.peekByteOff i idx
+getByte :: Integral a => B.Bits a => FS.Storable a => a -> Int -> W.Word8
+getByte i idx = fromInteger( toInteger(B.shiftL (B.shiftR i (8 * idx)) (8 * ((FS.sizeOf i) - idx))))
