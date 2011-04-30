333cc6b7ecd5783d73136ae8f7f56a2237654ce8
znation <znation@gmail.com>
2011-04-29 22:59:16 -0700

    actual generation, now running out of memory

--- Statistics.hs
+++ Statistics.hs
@@ -1,17 +1,24 @@
-module Statistics (Statistics, generate) where
+module Statistics (Statistics, generate, distribution) where
 
 import qualified Data.Map as Map
 import Data.Word
 
-type Statistics = Map.Map Word8 (Map.Map Word8 Int)
+-- Map<Word8, Tuple<Map<Word8, Int>, Int>> where
+-- the first Word8 is the current sample
+-- the next Word8 is the next sample
+-- the first Int is the number of occurrances of the sample pair
+-- the second Int is the total number of occurrances with the first sample
+type Statistics = Map.Map Word8 ((Map.Map Word8 Int), Int)
                     
 add :: Statistics -> Word8 -> Word8 -> Statistics
 add stats x y = if      (Map.member x stats)
                 then    let inner = stats Map.! x
-                        in  if      (Map.member y inner)
-                            then    Map.insert x (Map.insert y ((inner Map.! y) + 1) inner) stats
-                            else    Map.insert x (Map.insert y 1 inner) stats
-                else    Map.insert x (Map.insert y 1 Map.empty) stats
+                            innerMap = fst inner
+                            innerCount = snd inner
+                        in  if      (Map.member y innerMap)
+                            then    Map.insert x ((Map.insert y ((innerMap Map.! y) + 1) innerMap), innerCount+1) stats
+                            else    Map.insert x ((Map.insert y 1 innerMap), innerCount+1) stats
+                else    Map.insert x ((Map.insert y 1 Map.empty), 1) stats
                 
 generate :: [Word8] -> Statistics
 generate = generate' Map.empty
@@ -19,3 +26,27 @@ generate = generate' Map.empty
 generate' :: Statistics -> [Word8] -> Statistics
 generate' stats (x:y:z:xs) = generate' (add stats x y) (y:z:xs)
 generate' stats (x:y:[]) = add stats x y
+
+-- generates a list of (key, frequency) tuples
+percentages :: (Map.Map Word8 Int, Int) -> [(Word8, Double)]
+percentages (map, total) = percentages' (map, total) (Map.keys map) []
+
+percentages' :: (Map.Map Word8 Int, Int) -> [Word8] -> [(Word8, Double)] -> [(Word8, Double)]
+percentages' _ [] frequencies = frequencies
+percentages' (map, total) keys frequencies =    let key = head keys
+                                                    count = map Map.! key
+                                                    percentage = 100.0 * ((fromIntegral count) / (fromIntegral total))
+                                                in  percentages' (map, total) (tail keys) ((key, percentage):frequencies)
+
+-- generates a list of 100 items based on the distribution
+distribution :: (Map.Map Word8 Int, Int) -> [Word8]
+distribution (map, total) = distribution' (percentages (map, total)) []
+
+distribution' :: [(Word8, Double)] -> [Word8] -> [Word8]
+distribution' [] acc = acc
+distribution' pcts acc =    let current = head pcts
+                                key = fst current
+                                freq = snd current
+                            in  if (freq >= 1.0)
+                                then distribution' ((key, freq-1.0):(tail pcts)) (key:acc)
+                                else distribution' (tail pcts) (key:acc)
--- Synth.hs
+++ Synth.hs
@@ -1,20 +1,28 @@
 import System.IO
+import qualified System.Random as Rand
 import qualified Wave
 import qualified Statistics
 import qualified Data.Map as Map
 import Data.Word
 
 main :: IO ()
-main = do   input <- Wave.readFile "input.wav"
-            Wave.writeFile (fromStatistics input) "output.wav"
+main = do   gen <- Rand.newStdGen
+            input <- Wave.readFile "input.wav"
+            let randoms = Rand.randoms gen :: [Double]
+            Wave.writeFile (fromStatistics randoms input) "output.wav"
+           
+fromStatistics :: [Double] -> Wave.WaveFile -> Wave.WaveFile
+fromStatistics randoms input =  let stats = Statistics.generate (Wave.dataBytes input)
+                                    bytes = fromStatistics' randoms stats [] (length (Wave.dataBytes input))
+                                in  Wave.fromData bytes
 
-fromStatistics :: Wave.WaveFile -> Wave.WaveFile
-fromStatistics input = let  stats = Statistics.generate (Wave.dataBytes input)
-                            bytes = fromStatistics' stats [] (length (Wave.dataBytes input))
-                       in   Wave.fromData bytes
+fromStatistics' :: [Double] -> Statistics.Statistics -> [Word8] -> Int -> [Word8]
+fromStatistics' randoms stats bytes 0 = bytes
+fromStatistics' randoms stats bytes i = let newByte = generateByte (randoms !! i) (stats Map.! (last bytes))
+                                        in  fromStatistics' randoms stats (newByte : bytes) (i-1)
 
-fromStatistics' :: Statistics.Statistics -> [Word8] -> Int -> [Word8]
-fromStatistics' stats bytes 0 = bytes
-fromStatistics' stats bytes i = let newByte = 0
-                                in  fromStatistics' stats (newByte : bytes) (i-1)
-                                
+generateByte :: Double -> (Map.Map Word8 Int, Int) -> Word8
+generateByte threshold (map, count) =   let dist = Statistics.distribution (map, count)
+                                            idx = floor (threshold * 100.0)
+                                        in  dist !! idx
+
