4c92864e7f93320339e6792e63806656dc0b3ce3
znation <znation@gmail.com>
2011-05-08 22:24:53 -0700

    cleaning up warnings

--- Portfolio.hs
+++ Portfolio.hs
@@ -11,6 +11,7 @@ data Portfolio = Portfolio {    money :: Double,
 commission :: Double
 commission = 8.00
 
+startingMoney :: Double
 startingMoney = 10000.00
                                 
 value :: Double -> Portfolio -> Double
@@ -20,12 +21,11 @@ value' :: Double -> Double -> Lot -> Double
 value' curr m l = m + (curr * (fromIntegral (shares l)))
 
 buy :: Portfolio -> Quote.Quote -> Portfolio
-buy s q =   let startingMoney = money s
-                startingLots = lots s
+buy s q =   let startingLots = lots s
                 price = Quote.close q
-                numShares = floor ((startingMoney - commission) / price)
+                numShares = floor (((money s) - commission) / price)
                 lot = Lot price numShares
-            in  Portfolio (startingMoney - ((price * (fromIntegral numShares)) + commission)) (lot:startingLots)
+            in  Portfolio ((money s) - ((price * (fromIntegral numShares)) + commission)) (lot:startingLots)
             
 sell :: Portfolio -> Quote.Quote -> Portfolio
 sell s q =  Portfolio ((value (Quote.close q) s) - commission) []
