0f037edad1456a077c7acdb5d3c67f0060b6fbe2
znation <znation@gmail.com>
2011-05-17 20:10:37 -0700

    testing portfolio selling

--- Portfolio.hs
+++ Portfolio.hs
@@ -1,12 +1,27 @@
 module Portfolio where
 
 import qualified Quote
+import qualified Test.QuickCheck as QC
 
 data Lot = Lot {    pricePaid :: Double,
                     shares  :: Int  }
+    deriving Show
     
+instance QC.Arbitrary Lot where
+    arbitrary = do
+        p <- QC.arbitrary
+        s <- QC.arbitrary
+        return (Lot (abs p) ((abs s)+1))
+                    
 data Portfolio = Portfolio {    money :: Double,
                                 lots :: [Lot]   }
+    deriving Show
+    
+instance QC.Arbitrary Portfolio where
+    arbitrary = do
+        m <- QC.arbitrary
+        l <- QC.arbitrary
+        return (Portfolio (abs m) l)
 
 commission :: Double
 commission = 8.00
@@ -15,10 +30,10 @@ startingMoney :: Double
 startingMoney = 10000.00
                                 
 value :: Double -> Portfolio -> Double
-value curr p = foldl (value' curr) (money p) (lots p)
+value curr p = sum ((money p):(map (value' curr) (lots p)))
 
-value' :: Double -> Double -> Lot -> Double
-value' curr m l = m + (curr * (fromIntegral (shares l)))
+value' :: Double -> Lot -> Double
+value' curr l = curr * (fromIntegral (shares l))
 
 buy :: Portfolio -> Quote.Quote -> Portfolio
 buy s q =   let startingLots = lots s
@@ -29,3 +44,14 @@ buy s q =   let startingLots = lots s
             
 sell :: Portfolio -> Quote.Quote -> Portfolio
 sell s q =  Portfolio ((value (Quote.close q) s) - commission) []
+
+prop_sellReducesByCommission :: Portfolio -> Quote.Quote -> Bool
+prop_sellReducesByCommission p q =  let originalValue = value (Quote.close q) p
+                                        canSell = length (lots p) > 0
+                                        soldValue = value (Quote.close q) (sell p q)
+                                        
+                                    in  if      canSell
+                                        then    let padding = 0.0001 -- padding to within a hundredth of a cent (exact values are susceptible to floating point rounding)
+                                                    difference = originalValue - (soldValue + commission)
+                                                in  padding > (abs difference)
+                                        else    True
--- Testing.hs
+++ Testing.hs
@@ -1,9 +1,11 @@
+import qualified Portfolio
 import qualified Seed
 import qualified Test.QuickCheck as QC
 
 main :: IO ()
 main = do
     check Seed.prop_weightsBetweenZeroAndOne
+    check Portfolio.prop_sellReducesByCommission
 
 check :: QC.Testable a => a -> IO ()
-check x = QC.quickCheckWith (QC.stdArgs {QC.maxSuccess = 200}) x
+check x = QC.quickCheckWith (QC.stdArgs {QC.maxSuccess = 1000}) x
