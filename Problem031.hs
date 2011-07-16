b3c844b18d2dcbe56456a87449f064138fd63de4
Zach Nation <znation@gmail.com>
2011-07-16 09:58:12 -0700

    Problem31 refactoring, still broken

--- Problem031.hs
+++ Problem031.hs
@@ -1,7 +1,5 @@
 module Problem031 where
 
--- TODO -- this is doing permutations, not combinations
-
 import Data.List
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -42,13 +40,10 @@ type Range = [Integer]
 makeSolution :: Range -> Solution
 makeSolution r = Map.fromList (zip denominations r)
        
-combinations2 :: [Solution]
--- combinations2 = let ranges = [[a,b,c,d,e,f,g,h] | a <- [0..1], b <- [0..2], c <- [0..4], d <- [0..10], e <- [0..20], f <- [0..40], g <- [0..100], h <- [0..200]]
-                    -- solutions = map makeSolution ranges
-                -- in  filter valid solutions
-combinations2 = let firstSolution = Map.fromList [(200,1)]
+combinations :: [Solution]
+combinations =  let firstSolution = Map.fromList [(200,1)]
                     firstDenomination = 200
-                in  combine2 firstSolution firstDenomination
+                in  combine firstSolution firstDenomination
 
 lowerDenomination :: Integer -> Integer
 lowerDenomination 200 = 100
@@ -72,39 +67,18 @@ removeHighestDenomination s =   let ks = reverse (sort (Map.keys s))
                                     l = lowerDenomination k
                                 in  ((insertOrUpdate s k (-1)),l)
             
-combine2 :: Solution -> Integer -> [Solution]
-combine2 s x =  let sum' = ssum s
+combine :: Solution -> Integer -> [Solution]
+combine s x =   let sum' = ssum s
                     (s',x') = removeHighestDenomination s
-                    rest = combine2 s' x'
+                    rest = combine s' x'
                 in  if      lastSolution s
                     then    [s]
                     else    if      sum' < result
-                            then    combine2 (insertOrUpdate s x 1) x
+                            then    combine (insertOrUpdate s x 1) x
                             else    if      sum' > result
                                     then    rest
                                     else    s:rest
                 
 denominations :: [Integer]
 denominations = reverse [1, 2, 5, 10, 20, 50, 100, 200]
-
-combinations :: Integer
-combinations = sum (map firstCombine denominations)
-
-firstCombine :: Integer -> Integer
-firstCombine x =    let y = x `debug` "Starting with"
-                    in  combine 0 y
-
-combine :: Integer -> Integer -> Integer
-combine = Memo.memo2 Memo.integral Memo.integral combine'
-
--- returns combinations that add up to 2 pounds
-combine' :: Integer -> Integer -> Integer
-combine' acc x =    let total = acc + x
-                        diff = result - total
-                        ds = filter (<= diff) denominations
-                    in  if      diff == 0
-                        then    1
-                        else    if      diff < 0
-                                then    0
-                                else    sum (map (combine total) ds)
 
